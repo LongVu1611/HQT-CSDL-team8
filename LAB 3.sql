7d7868bcb723b0fa179e9c705e81bdd66f65eb77
zzalobeiuzz <84489611+zzalobeiuzz@users.noreply.github.com>
2022-11-21 10:44:49 +0700

    Update LAB 3.sql

--- LAB 3.sql
+++ LAB 3.sql
@@ -1,10 +1,12 @@
-﻿-----------------------Xuất định dạng “tổng số giờ làm việc” kiểu decimal với 2 số thập phân.-----------------
-SELECT MADA AS 'MÃ ĐỒ ÁN' ,CAST (SUM(THOIGIAN) AS DECIMAL(5,2) ) AS'TỔNG SỐ GIỜ LÀM VIỆC'
-FROM PHANCONG
-GROUP BY MADA
+-----------------------Xuất định dạng “tổng số giờ làm việc” kiểu decimal với 2 số thập phân.-----------------
+SELECT TEN_CONG_VIEC AS 'TÊN CÔNG VIỆC' ,CAST (SUM(THOIGIAN) AS DECIMAL(5,2) ) AS'TỔNG SỐ GIỜ LÀM VIỆC'
+FROM PHANCONG,CONGVIEC
+WHERE PHANCONG.MADA=CONGVIEC.MADA
+GROUP BY TEN_CONG_VIEC
 GO
 -----------------------Xuất định dạng “tổng số giờ làm việc” kiểu varchar---------------
-SELECT MADA AS 'MÃ ĐỒ ÁN' ,CAST (SUM(THOIGIAN) AS varchar ) AS'TỔNG SỐ GIỜ LÀM VIỆC'
-FROM PHANCONG
-GROUP BY MADA
+SELECT TEN_CONG_VIEC AS 'TÊN CÔNG VIỆC' ,CAST (SUM(THOIGIAN) AS varchar ) AS'TỔNG SỐ GIỜ LÀM VIỆC'
+FROM PHANCONG,CONGVIEC
+WHERE PHANCONG.MADA=CONGVIEC.MADA
+GROUP BY TEN_CONG_VIEC
 GO
