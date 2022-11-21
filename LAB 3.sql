af9a047ca0c1e4afaac0bc9a3d7f316847caec09
zzalobeiuzz <84489611+zzalobeiuzz@users.noreply.github.com>
2022-11-21 10:40:15 +0700

    Add files via upload

---
+++ LAB 3.sql
@@ -0,0 +1,10 @@
+﻿-----------------------Xuất định dạng “tổng số giờ làm việc” kiểu decimal với 2 số thập phân.-----------------
+SELECT MADA AS 'MÃ ĐỒ ÁN' ,CAST (SUM(THOIGIAN) AS DECIMAL(5,2) ) AS'TỔNG SỐ GIỜ LÀM VIỆC'
+FROM PHANCONG
+GROUP BY MADA
+GO
+-----------------------Xuất định dạng “tổng số giờ làm việc” kiểu varchar---------------
+SELECT MADA AS 'MÃ ĐỒ ÁN' ,CAST (SUM(THOIGIAN) AS varchar ) AS'TỔNG SỐ GIỜ LÀM VIỆC'
+FROM PHANCONG
+GROUP BY MADA
+GO
