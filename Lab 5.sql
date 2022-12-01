aad4dc7d2bfb41afc1d0353a5e22dab0d7182048
Tran Quoc Huy SVIP VIP 12 <84489611+zzalobeiuzz@users.noreply.github.com>
2022-12-01 15:49:49 +0700

    Create Lab 5.sql

--- Lab 5.sql
+++ Lab 5.sql
@@ -1,4 +1,4 @@
-------------Câu 1 a)In ra dòng ‘Xin chào’ + @ten với @ten là tham số đầu vào là tên Tiếng Việt có dấu-------------
+---------------------Câu 1--------------------
 CREATE PROC XINCHAO
 @TEN NVARCHAR (20)
 AS
@@ -7,7 +7,7 @@ PRINT N'XIN CHÀO ' + @TEN;
 END
 EXEC XINCHAO N'NHÓM 8'
 GO
--------------Câu 1 b)Nhập vào 2 số @s1,@s2. In ra câu ‘Tổng là : @tg’ với @tg=@s1+@s2.---------------
+
 CREATE PROC TONG
 @S1 INT , @S2 INT
 AS
@@ -16,7 +16,6 @@ SET @TG = @S1+@S2
 PRINT CONCAT ('TONG LA:' , @TG)
 EXEC TONG 4,5
 GO
----------------Câu 1 c)Nhập vào số nguyên @n. In ra tổng các số chẵn từ 1 đến @n.
 
 CREATE PROC TONGSOCHAN
 @N INT 
@@ -31,7 +30,6 @@ PRINT'TONG' + CAST (@TONG AS VARCHAR(4))
 EXEC TONGSOCHAN 8
 GO
 
-------------Câu 1 d)Nhập vào 2 số. In ra ước chung lớn nhất của chúng-------
 CREATE PROC UOCCHUNG
 @A INT ,@B INT 
 AS
@@ -47,7 +45,9 @@ SELECT @C
 
 GO
 
-CREATE PROC THONGTINNV
+---------------------Câu 2--------------------
+-------------Nhập vào @Manv, xuất thông tin các nhân viên theo @Manv.---------------------
+CREATE PROC CAU2A
 	@MANV nvarchar(9)
 AS
 BEGIN
@@ -57,29 +57,68 @@ END
 
 GO
 
-CREATE PROC NVTHAMGIA
+EXEC CAU2A '003'
+GO
+
+-------------Nhập vào @MaDa (mã đề án), cho biết số lượng nhân viên tham gia đề án đó---------------
+CREATE PROC CAU2B
 	@MADA INT
 AS
 BEGIN
-	SELECT COUNT(MA_NVIEN) FROM PHANCONG
+	SELECT COUNT(MA_NVIEN) AS 'SỐ LƯỢNG NHÂN VIÊN THAM GIA ĐỀ ÁN' FROM PHANCONG
 	WHERE MADA = @MADA
 END
----------------------câu 2d----------------------
-create proc cau2_d @Trphg nvarchar(10) as
-begin
-select  * from NHANVIEN inner join PHONGBAN ON NHANVIEN.PHG = PHONGBAN.MAPHG
-where TRPHG = @Trphg
-end
-go
-exec cau2_d '006'
+GO
 
--------------Câu 2e--------------
-create proc cau2_e @Manv nvarchar(10),@Mapb nvarchar(10) as
-begin
-if exists (select * from NHANVIEN where MANV = @Manv and PHG = @Mapb)
-	print' Có'
-else
-	print ' Không'
-end
-go
-exec cau2_e '002','4'
+EXEC CAU2B'10'
+GO
+
+-------------Nhập vào @MaDa và @Ddiem_DA (địa điểm đề án), cho biết số lượng nhân viên tham gia đề án có mã đề án là @MaDa và địa điểm đề án là @Ddiem_DA------------------
+CREATE PROC CAU2C 
+	@MADA INT,@DDIEM_DA NVARCHAR(15) 
+AS 
+BEGIN 
+	SELECT COUNT(MANV) AS 'SỐ LƯỢNG NHÂN VIÊN' , MADA AS 'MÃ ĐỀ ÁN', TENPHG AS 'MÃ ĐỀ ÁN',DDIEM_DA AS 'ĐỊA ĐIỂM ĐỀ ÁN'FROM PHONGBAN 
+	INNER JOIN NHANVIEN ON NHANVIEN.PHG = PHONGBAN.MAPHG 
+	INNER JOIN DEAN ON DEAN.PHONG = PHONGBAN.MAPHG WHERE MADA = @MADA AND DDIEM_DA = @DDIEM_DA 
+	GROUP BY TENPHG,MADA,DDIEM_DA 
+END 
+GO 
+
+EXEC CAU2C 3,'TP HCM'
+GO
+
+
+-------------------Nhập vào @Trphg (mã trưởng phòng), xuất thông tin các nhân viên có trưởng phòng là @Trphg và các nhân viên này không có thân nhân.---------------
+CREATE PROC CAU2D 
+	@TRPHG NVARCHAR(10) 
+AS 
+BEGIN 
+	SELECT * FROM NHANVIEN 
+	INNER JOIN PHONGBAN ON NHANVIEN.PHG = PHONGBAN.MAPHG 
+	WHERE TRPHG = @TRPHG 
+END 
+GO 
+
+EXEC CAU2D '006'
+GO
+
+----------------------Nhập vào @Manv và @Mapb, kiểm tra nhân viên có mã @Manv có thuộc phòng ban có mã @Mapb hay không----------------
+CREATE PROC CAU2E 
+	@MANV NVARCHAR(10),@MAPB NVARCHAR(10) 
+AS
+BEGIN
+IF EXISTS (SELECT * FROM NHANVIEN WHERE MANV = @MANV AND PHG = @MAPB)
+	PRINT' CÓ'
+ELSE
+	PRINT ' KHÔNG'
+END
+GO
+
+EXEC CAU2E '002','4'
+GO
+
+---------------------- Câu 3 -----------------------------
+-------------------Thêm phòng ban có tên CNTT vào csdl QLDA, các giá trị được thêm vào dưới dạng tham số đầu vào, kiếm tra nếu trùng Maphg thì thông báo thêm thất bại.------------------
+
+
